7fe99e9a14cdcb6cea6972c995a73c5fe5a41505
Mikhail879 <sekasniy@list.ru>
2022-02-22 22:40:02 +0600

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) {{
+  let result = '';
+  let a = Math.abs(n);
+  let b = a.toString();
+  for (let i = b.length - 1; i >= 0; i--)
+  result = +`${result}${b[i]}`;
+  return result;
+}
 }
